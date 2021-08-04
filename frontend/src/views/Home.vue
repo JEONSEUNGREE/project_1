d38a58b60f10f10468c96acdb4e07d1d1c058e29
JEONSEUNGREE <jws08219@naver.com>
2021-08-04 21:11:53 +0900

    install vuetify

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-  </div>
+  <hello-world />
 </template>
 
 <script>
+  import HelloWorld from '../components/HelloWorld'
 
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
+</script>
