ea7e6063163f373c1d40bd4eaf7af4bc912dd382
JEONSEUNGREE <jws08219@naver.com>
2021-08-15 00:44:51 +0900

    "feature/projectPlan"

--- frontend/src/store/modules/index.js
+++ frontend/src/store/modules/index.js
@@ -3,11 +3,13 @@ import userInfor from './userInfor'
 import board from './board'
 import contact from './contact'
 import authentication from './authentication'
+import project from './project'
 
 export default {
   drawer,
   userInfor,
   board,
   contact,
-  authentication
+  authentication,
+  project
 }
