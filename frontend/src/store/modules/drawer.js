39e92dd845298961414b4f2a72b82ae941a2f136
JEONSEUNGREE <jws08219@naver.com>
2021-08-18 17:55:20 +0900

    modify-icon

--- frontend/src/store/modules/drawer.js
+++ frontend/src/store/modules/drawer.js
@@ -2,16 +2,16 @@ const state = {
   namespaced: true,
   drawer: false,
   items: [
-    { title: 'Home', icon: 'mdi-view-dashboard', to: '/home'},
+    { title: 'Home', icon: 'mdi-home', to: '/home'},
     { title: 'T&A', icon: 'mdi-image', items:[
-      { title: 'Schedule', icon: 'mdi-view-dashboard', to: '/schedule'},
-      { title: 'Commuting Record', icon: 'mdi-view-dashboard', to: '/commuting'},
+      { title: 'Schedule', icon: 'mdi-clock-outline', to: '/schedule'},
+      { title: 'Calendar', icon: 'mdi-calendar-check', to: '/commuting'},
     ]},
-    { title: 'Organization', icon: 'mdi-view-dashboard', items:[
-      { title: 'Employee Contact List', icon: 'mdi-view-dashboard', to: '/contact-list'},
-      { title: 'Project Plan', icon: 'mdi-view-dashboard', to: '/plan'},
+    { title: 'Organization', icon: 'mdi-account-group', items:[
+      { title: 'Employee Contact List', icon: 'mdi-card-account-phone', to: '/contact-list'},
+      { title: 'Project Plan', icon: 'mdi-projector-screen', to: '/plan'},
     ]},
-    { title: 'Board', icon: 'mdi-view-dashboard', to: '/board'}
+    { title: 'Board', icon: 'mdi-billboard', to: '/board'}
   ]
 
 }
